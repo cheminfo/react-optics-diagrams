8a17525d4b1c2727947e2dc0c4ebd7df8e6e88db
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2025-06-11 11:55:06 +0200

    fix: Export RayIntersections type

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export type { SpotDiagramProps, SpotDiagramsGridProps } from './components';
 export type {
   BoundingBox,
   FieldSpec,
+  RayIntersections,
   RayTraceResult,
   RayTraceResults,
   SpotDiagramOptions,
